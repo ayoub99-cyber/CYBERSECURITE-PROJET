d425811c1bdad450d0d034b22e25e2ea03c485c1
ayoub99-cyber <ayoubltbr@gmail.com>
2026-04-16 19:28:25 +0200

    Version ultérieurs du code

--- Déploiement.py
+++ Déploiement.py
@@ -28,10 +28,6 @@ result = input("Souhaitez-vous afficher les logs pour Windows(1) ou Linux(2) ? (
 if result == "1":   afficher_logs_windows()
 elif result == "2":   afficher_logs_linux()
 else:   print("Choix invalide Veuilliez Réssayez .")
-   # Récuperation des Array de services critiques pour les deux systèmes
+   # Array de services critiques pour les deux systèmes
 services_critiques_windows = ['Windows Update', 'Microsoft Defender Antivirus Service', 'Windows Event Log']
 services_critiques_linux = ['ssh.service', 'nginx.service', 'mysql.service']
-print("\nServices critiques pour Windows:")
-for service in services_critiques_windows:  print(f"- {service}")
-print("\nServices critiques pour Linux:")   
-for service in services_critiques_linux:    print(f"- {service}")
